314d026f5f7292dd48840410207045223f485d0e
pichus <dimatyzuk@gmail.com>
2025-11-11 19:01:53 +0200

    fix formatting + add localization + fix bugs with vectors

--- src/main.cpp
+++ src/main.cpp
@@ -1,35 +1,44 @@
 #include <barrier>
 #include <format>
 #include <iostream>
+#include <list>
 #include <map>
+#include <memory>
 #include <syncstream>
 #include <thread>
 #include <vector>
-#include <memory>
+
+#ifdef _WIN32
+#include <windows.h>
+#endif
 
 constexpr int kThreadCount = 5;
 
 void PerformComputations(const char set_name, const int action_number) {
-    std::osyncstream(std::cout)
-            << std::format("З набору {}, виконано дію {}.\n", set_name, action_number);
+    std::osyncstream(std::cout) << std::format(
+        "З набору {}, виконано дію {}.\n", set_name, action_number);
     std::this_thread::sleep_for(std::chrono::seconds(1));
 }
 
-void RunMultithreaded(const std::vector<std::string> &thread_tasks, std::map<char, int> &set_action_current_ids) {
-    std::vector<std::jthread> threads(kThreadCount);
+void RunMultithreaded(const std::vector<std::string> &thread_tasks,
+                      std::map<char, int> &set_action_current_ids) {
+    std::list<std::jthread> threads;
+
     std::barrier sync_point(kThreadCount);
 
-    std::vector<std::vector<std::pair<char, int> > > set_actions;
+    std::vector<std::vector<std::pair<char, int>>> set_actions(
+        kThreadCount, std::vector<std::pair<char, int>>());
 
     for (int i = 0; i < kThreadCount; i++) {
-        for (const char &set_name: thread_tasks[i]) {
-            set_actions[i].emplace_back(set_name, set_action_current_ids[set_name]++);
+        for (const char &set_name : thread_tasks[i]) {
+            set_actions[i].emplace_back(set_name,
+                                        set_action_current_ids[set_name]++);
         }
     }
 
     for (int thread_i = 0; thread_i < kThreadCount; thread_i++) {
-        threads.emplace_back([&set_actions, &thread_i, &sync_point]() {
-            for (const auto [set_name, action_id]: set_actions[thread_i]) {
+        threads.emplace_back([set_actions, thread_i, &sync_point]() {
+            for (const auto [set_name, action_id] : set_actions[thread_i]) {
                 if (set_name != ' ') {
                     PerformComputations(set_name, action_id);
                 }
@@ -39,43 +48,28 @@ void RunMultithreaded(const std::vector<std::string> &thread_tasks, std::map<cha
     }
 }
 
-
 int main() {
+#ifdef _WIN32
+    SetConsoleOutputCP(CP_UTF8);
+    SetConsoleCP(CP_UTF8);
+#endif
+
     std::cout << "Обчислення розпочато\n";
 
     std::map<char, int> set_action_current_ids;
 
     for (int i = 0; i < 10; i++) {
-        char set_name = 'a' + i; // convert i from int to char
+        char set_name = 'a' + i;  // convert i from int to char
         set_action_current_ids[set_name] = 1;
     }
 
-    std::vector<std::string> thread_tasks = {
-        "aabcdeeffhiijj",
-        "abccdeefghi jj",
-        "abccdeffghi jj",
-        "abccdeffghi j ",
-        "abcdeffghi j "
-    };
+    std::vector<std::string> thread_tasks = {"aabcdeeffhiijj", "abccdeefghi jj",
+                                             "abccdeffghi jj", "abccdeffghi j ",
+                                             "abcdeeffghi j "};
 
     RunMultithreaded(thread_tasks, set_action_current_ids);
 
-
     std::cout << "Обчислення завершено\n";
-
-
-    // const std::map<std::string, int> set_actions = {
-    //     {"a", 6},
-    //     {"b", 5},
-    //     {"c", 8},
-    //     {"d", 5},
-    //     {"e", 8},
-    //     {"f", 9},
-    //     {"g", 4},
-    //     {"h", 5},
-    //     {"i", 6},
-    //     {"j", 8}
-    // };
 
     return 0;
 }
